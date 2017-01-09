36b99f20d23059f852ca790046d5b513bf6492c0
urs8000 <ursm@gmx.net>
2017-01-09 18:46:42 +0100

    final version

--- master_bit_3.py
+++ master_bit_3.py
@@ -32,17 +32,26 @@ SerialInit()
 sleep(2000)
 microbit_write_UART()
 display.show(Image.DIAMOND_SMALL)
+wait_4_send = 0
 
 
 while True:
-    Message = microbit_read_UART()
-    if Message:
+    if wait_4_send == 0:
+        Message = microbit_read_UART()
+
+    if Message and wait_4_send == 0:
         uart.write("ready to send... press button A to transmit, B to clear")
         uart.write("\r\n")
         uart.write(Message)       # echo while using the FTDI
         uart.write("\r\n")
         display.show(Image.BUTTERFLY)
-        sleep(1000)
+        wait_4_send = 1
+
+    if button_a.is_pressed():
+        wait_4_send = 2
+        display.clear()
+
+    if wait_4_send == 2:           # and button_a.was_pressed():
         uart.write("... sending")
         uart.write("\r\n")
         radio.send_bytes(Message)
@@ -50,10 +59,11 @@ while True:
         sleep(2000)
         Message = ""
         display.clear()
+        wait_4_send = 0
 
-#        if button_b.is_pressed:
-#            Message = ""
-#            uart.write("... message cleared. enter a new one")
-#            uart.write("\r\n")
-#            display.clear()
-#            break
+    if button_b.is_pressed() and wait_4_send == 1:
+        Message = ""
+        uart.write("... message cleared. enter a new one")
+        uart.write("\r\n")
+        display.clear()
+        wait_4_send = 0
